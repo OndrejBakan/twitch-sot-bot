42ebe9a0a7f440edd54c75c4e0d81af07b0810bc
Ondrej Bakan <ondrej@bakan.cz>
2023-02-27 23:19:48 +0100

    Celý jsem to rozbil, achjo.

--- cogs/sot.py
+++ cogs/sot.py
@@ -1,15 +1,62 @@
 import aiohttp
-from twitchio.ext import commands
+from twitchio.ext import commands, routines
 
 
-class Test(commands.Cog):
+class AnnouncementsCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+    
+    @commands.Cog.event('event_channel_joined')
+    async def channel_joined(self, channel):
+        self.announce.start(channel)
 
+    @routines.routine(seconds=self.config.get('bot', 'sot.announcements.interval'))
+    async def announce(self, ctx):
+        content = 'Vyzkoušej příkazy: !reputation | !balance | !winrate'
+
+        await ctx.send(content=content)
+
+
+class BalanceCog(commands.Cog):
+    def __init__(self, bot: commands.Bot, http_client: aiohttp.ClientSession):
+        self.bot = bot
+        self.http_client = http_client
+
+
+class ReputationCog(commands.Cog):
+    def __init__(self, bot: commands.Bot, http_client: aiohttp.ClientSession):
+        self.bot = bot
+        self.http_client = http_client
+    
     @commands.command()
     @commands.cooldown(rate=1, per=120, bucket=commands.Bucket.channel)
     async def reputation(self, ctx: commands.Context):
-        await ctx.send('Halo')
+        if self.config.getboolean('bot', 'sot.reputation.enabled'):
+            await ctx.send('Reputation')
+
+
+class WinrateCog(commands.Cog):
+    def __init__(self, bot: commands.Bot, http_client: aiohttp.ClientSession):
+        self.bot = bot
+        self.http_client = http_client
+
 
 def prepare(bot: commands.Bot):
-    bot.add_cog(Test(bot))
+
+    http_client = aiohttp.ClientSession(
+        cookies = {
+            'rat': bot.config.get('seaofthieves.com', 'rat')
+        }
+    )
+
+    if bot.config.getboolean('bot', 'sot.announcements.enabled', fallback=False):
+        bot.add_cog(AnnouncementsCog(bot))
+    
+    if bot.config.getboolean('bot', 'sot.balance.enabled', fallback=False):
+        bot.add_cog(BalanceCog(bot, http_client))
+    
+    if bot.config.getboolean('bot', 'sot.reputation.enabled', fallback=False):
+        bot.add_cog(ReputationCog(bot, http_client))
+    
+    if bot.config.getboolean('bot', 'sot.winrate.enabled', fallback=False):
+        bot.add_cog(WinrateCog(bot, http_client))
--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from twitchio.ext import commands
 class Bot(commands.Bot):
 
     def __init__(self):
-        config = ConfigParser()
-        config.read('config.ini')
+        self.config = ConfigParser()
+        self.config.read('config.ini')
 
-        super().__init__(token=config.get('twitch', 'token'),
-                         prefix=config.get('bot', 'prefix'),
-                         initial_channels=[config.get('twitch', 'channel')])
+        super().__init__(token=self.config.get('twitch', 'token'),
+                         prefix=self.config.get('bot', 'prefix'),
+                         initial_channels=[self.config.get('twitch', 'channel')])
         
         # load module
         if (
-            config.getboolean('bot', 'sot.balance.enabled') or
-            config.getboolean('bot', 'sot.reputation.enabled') or
-            config.getboolean('bot', 'sot.winrate.enabled')
+            self.config.getboolean('bot', 'sot.announcements.enabled') or
+            self.config.getboolean('bot', 'sot.balance.enabled') or
+            self.config.getboolean('bot', 'sot.reputation.enabled') or
+            self.config.getboolean('bot', 'sot.winrate.enabled')
         ):
             self.load_module('cogs.sot')
 
